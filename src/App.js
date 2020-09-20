b959099ac2d208627f4135c6695937b3acbfb5eb
Sachin Shukla <sachin.shukla@deskera.com>
2020-09-20 16:35:39 +0800

    Adding Routers

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ import {
   MaterialTableComponent7_partiallyControlledComponent,
   MaterialTableComponent8_editColumnStateComponent,
 } from "./components/material-table-component";
+import AppRouters from "./routers";
 
 function App() {
   var disPlayLogo = false;
   return (
     <div className="App">
+      {/* <HeaderComponent /> */}
+      <AppRouters />
       {/* <MaterialTableComponent /> */}
       {/* <MaterialTableComponent2_customizeTable /> */}
       {/* <MaterialTableComponent3_cellComponent /> */}
@@ -26,11 +29,31 @@ function App() {
       {/* <MaterialTableComponent5_contentComponent />*/}
       {/* <MaterialTableComponent6_controlledComponent /> */}
       {/* <MaterialTableComponent7_partiallyControlledComponent /> */}
-      <MaterialTableComponent8_editColumnStateComponent />
-      {disPlayLogo && <DisplayLogoFn />}
+      {/* <MaterialTableComponent8_editColumnStateComponent /> */}
+      {/* {disPlayLogo && <DisplayLogoFn />} */}
     </div>
   );
 }
+
+// const HeaderComponent = () => {
+//   return (
+//     <div>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to="/t1">t1</Link>
+//           </li>
+//           <li>
+//             <Link to="/t2">t2</Link>
+//           </li>
+//           <li>
+//             <Link to="/t3">t3</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// };
 
 const DisplayLogoFn = () => {
   return (
--- src/routers.js
+++ src/routers.js
@@ -1,46 +1,68 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function Routers() {
+import {
+  MaterialTableComponent,
+  MaterialTableComponent2_customizeTable,
+  MaterialTableComponent3_cellComponent,
+  MaterialTableComponent4_rowComponent,
+  MaterialTableComponent5_contentComponent,
+  MaterialTableComponent6_controlledComponent,
+  MaterialTableComponent7_partiallyControlledComponent,
+  MaterialTableComponent8_editColumnStateComponent,
+} from "./components/material-table-component";
+
+export default function AppRouters() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/t1">t1</Link>
+          </li>
+          <li>
+            <Link to="/t2">t2</Link>
+          </li>
+          <li>
+            <Link to="/t3">t3</Link>
+          </li>
+          <li>
+            <Link to="/t4">t4</Link>
+          </li>
+          <li>
+            <Link to="/t5">t5</Link>
+          </li>
+          <li>
+            <Link to="/t6">t6</Link>
+          </li>
+          <li>
+            <Link to="/t7">t7</Link>
+          </li>
+          <li>
+            <Link to="/t8">t8</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+      <Route path="/t1" component={MaterialTableComponent} />
+      <Route path="/t2" component={MaterialTableComponent2_customizeTable} />
+      <Route path="/t3" component={MaterialTableComponent3_cellComponent} />
+      <Route path="/t4" component={MaterialTableComponent4_rowComponent} />
+      <Route path="/t5" component={MaterialTableComponent5_contentComponent} />
+      <Route path="/t6" component={MaterialTableComponent6_controlledComponent} />
+      <Route path="/t7" component={MaterialTableComponent7_partiallyControlledComponent} />
+      <Route path="/t8" component={MaterialTableComponent8_editColumnStateComponent} />
     </Router>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
+function Tbl8() {
+  return MaterialTableComponent8_editColumnStateComponent;
 }
 
-function About() {
-  return <h2>About</h2>;
+function Tbl2() {
+  return MaterialTableComponent2_customizeTable;
 }
 
 function Users() {
